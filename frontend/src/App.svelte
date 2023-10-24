c5a1184469856d7a9f01402ce1f8e4e16b30a563
Meow <me@itsmeow.cat>
2023-10-23 23:46:48 -0400

    sveltekit

--- frontend/src/App.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import { Route, Router } from "svelte-routing";
-  import Home from "./pages/home/Home.svelte";
-</script>
-
-<Router>
-  <Route path="/" component={Home} />
-</Router>
