73b590c1ca76aeb63d1ad4a29fa26000ca6f6d3d
Meow <me@itsmeow.cat>
2023-10-25 15:20:15 -0400

    artist page virtual list

--- frontend/src/routes/server/[domain]/[library]/[primaryID]/+page.svelte
+++ frontend/src/routes/server/[domain]/[library]/[primaryID]/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-	import { page } from "$app/stores";
 	import ArtistPage from "./ArtistPage.svelte";
 </script>
-
-page for primary (artist, tv show, movie) {$page.params.primaryID}
 
 <ArtistPage />
--- frontend/src/routes/server/[domain]/[library]/[primaryID]/ArtistPage.svelte
+++ frontend/src/routes/server/[domain]/[library]/[primaryID]/ArtistPage.svelte
@@ -4,35 +4,42 @@
 	import { VirtualScroll } from "svelte-virtual-scroll-list";
 
 	let innerWidth = 0,
-		offsetWidth = 0;
+		offsetWidth = 0,
+		offsetHeight = 0;
 
 	const toasts = getToastStore();
 </script>
 
 <svelte:window bind:innerWidth />
 
-<div bind:offsetWidth>
-	<VirtualScroll
-		data={new Array(1500).fill(null).map(() => ({
-			id: Math.random(),
-			card: new Array(innerWidth > 640 ? Math.floor(offsetWidth / 128) : 3)
-				.fill(null)
-				.map(() => ({ id: Math.random() })),
-		}))}
-		key="id"
-		let:data
-		let:index
-	>
-		<div class="flex sm:gap-4 sm:mb-2 gap-2 mb-1 items-center justify-center">
-			{#each data.card as album (album.id)}
-				<LibraryCardMusic
-					name="MYCELiUM {index}"
-					href="/server/flixur.app/music/artistid/albumid"
-					image="https://i.scdn.co/image/ab67616d0000b273791e5ad29bbd6253aa453293"
-					on:play={() => toasts.trigger({ message: "play button clicked" })}
-					on:edit={() => toasts.trigger({ message: "edit button clicked" })}
-				/>
-			{/each}
-		</div>
-	</VirtualScroll>
+<div class="h-full" bind:offsetHeight>
+	<div class="vs" style:height="{offsetHeight}px" bind:offsetWidth>
+		<VirtualScroll
+			data={new Array(1500).fill(null).map(() => ({
+				id: Math.random(),
+				card: new Array(innerWidth > 640 ? Math.floor(offsetWidth / 128) : 3)
+					.fill(null)
+					.map(() => ({ id: Math.random() })),
+			}))}
+			let:data
+		>
+			<div class="flex sm:gap-4 sm:py-1 gap-2 py-0.5 items-center justify-center snap-start">
+				{#each data.card as album (album.id)}
+					<LibraryCardMusic
+						name="MYCELiUM"
+						href="/server/flixur.app/music/artistid/albumid"
+						image="https://i.scdn.co/image/ab67616d0000b273791e5ad29bbd6253aa453293"
+						on:play={() => toasts.trigger({ message: "play button clicked" })}
+						on:edit={() => toasts.trigger({ message: "edit button clicked" })}
+					/>
+				{/each}
+			</div>
+		</VirtualScroll>
+	</div>
 </div>
+
+<style>
+	.vs :global(.virtual-scroll-root) {
+		@apply snap-y snap-mandatory scroll-smooth;
+	}
+</style>
