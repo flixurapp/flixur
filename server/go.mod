470ced41a90804b901cdc4f609da27e25702ddca
Meow <me@itsmeow.cat>
2024-05-17 16:12:12 -0400

    logging

--- server/go.mod
+++ server/go.mod
@@ -7,9 +7,13 @@ require (
 	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/chi/v5 v5.0.12
 	github.com/go-chi/cors v1.2.1
+	github.com/rs/zerolog v1.32.0
 )
 
 require (
 	github.com/fxamacker/cbor/v2 v2.6.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 )
--- server/server.go
+++ server/server.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 var port = 8787
 
 func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{
+		Out:        os.Stdout,
+		TimeFormat: "3:04:05PM",
+	})
+
 	router := chi.NewMux()
 	router.Use(middleware.Compress(5))
 	router.Use(middleware.Recoverer)
@@ -21,10 +28,9 @@ func main() {
 	RegisterAPI(router)
 
 	if Dev {
-		log.Print("Running in development mode.")
-	} else {
-		log.Printf("Listening on port %d.", port)
+		log.Info().Msg("Running in development mode.")
 	}
+	log.Info().Int("port", port).Msg("Server is online.")
 
 	http.ListenAndServe(fmt.Sprintf(":%d", port), router)
 }
