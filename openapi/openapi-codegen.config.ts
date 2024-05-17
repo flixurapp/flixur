db61a28400565bb7f9fca11a353219633189367c
Meow <me@itsmeow.cat>
2024-05-17 15:10:57 -0400

    schema generation

---
+++ openapi/openapi-codegen.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "@openapi-codegen/cli";
+import { generateFetchers, generateSchemaTypes } from "@openapi-codegen/typescript";
+export default defineConfig({
+  flixur: {
+    from: {
+      source: "url",
+      url: "http://localhost:8888/api/openapi.json",
+    },
+    outputDir: "api",
+    to: async (context) => {
+      const filenamePrefix = "flixur";
+      const { schemasFiles } = await generateSchemaTypes(context, {
+        filenamePrefix,
+      });
+      await generateFetchers(context, {
+        filenamePrefix,
+        schemasFiles,
+      });
+    },
+  },
+});
