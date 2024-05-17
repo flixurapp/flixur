459a64e9dca1d3dff5d892061a0ae2e87188b446
Meow <me@itsmeow.cat>
2024-05-17 00:01:37 -0400

    tygo and proto changes

--- proto.sh
+++ proto.sh
@@ -1,2 +1,3 @@
 # Compile all protobufs for go.
 protoc -I=./proto --go_out=./proto/go --go_opt=paths=source_relative ./proto/artist.proto
+sed -i '/\*.*omitempty/!s/,omitempty//' ./proto/go/*.pb.go
