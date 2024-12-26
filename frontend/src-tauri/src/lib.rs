00d53e01d3e6a0009580e47a38decf35591247d2
Meow <me@itsmeow.cat>
2024-12-26 03:47:55 -0500

    fix

--- frontend/src-tauri/src/lib.rs
+++ frontend/src-tauri/src/lib.rs
@@ -1,4 +1,6 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  // your code here
+  tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
