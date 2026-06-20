0144c5431d9089cc3a517414208bfc787f4c3e5f
Meow <github@xela.codes>
2026-06-20 14:34:14 -0400

    upgrade nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "flixur project";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     go-overlay = {
