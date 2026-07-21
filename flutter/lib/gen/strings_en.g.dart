a09a090f2ac6a45e82396ee97649ab432ed5d4ce
Meow <github@xela.codes>
2026-07-21 00:01:12 -0400

    translations / app screen

---
+++ flutter/lib/gen/strings_en.g.dart
@@ -0,0 +1,88 @@
+///
+/// Generated file. Do not edit.
+///
+// coverage:ignore-file
+// ignore_for_file: type=lint, unused_import
+// dart format off
+
+part of 'strings.g.dart';
+
+// Path: <root>
+typedef TranslationsEn = Translations; // ignore: unused_element
+class Translations with BaseTranslations<AppLocale, Translations> {
+	/// Returns the current translations of the given [context].
+	///
+	/// Usage:
+	/// final t = Translations.of(context);
+	static Translations of(BuildContext context) => InheritedLocaleData.of<AppLocale, Translations>(context).translations;
+
+	/// You can call this constructor and build your own translation instance of this locale.
+	/// Constructing via the enum [AppLocale.build] is preferred.
+	Translations({Map<String, Node>? overrides, PluralResolver? cardinalResolver, PluralResolver? ordinalResolver, TranslationMetadata<AppLocale, Translations>? meta})
+		: assert(overrides == null, 'Set "translation_overrides: true" in order to enable this feature.'),
+		  $meta = meta ?? TranslationMetadata(
+		    locale: AppLocale.en,
+		    overrides: overrides ?? {},
+		    cardinalResolver: cardinalResolver,
+		    ordinalResolver: ordinalResolver,
+		  ) {
+		$meta.setFlatMapFunction(_flatMapFunction);
+	}
+
+	/// Metadata for the translations of <en>.
+	@override final TranslationMetadata<AppLocale, Translations> $meta;
+
+	/// Access flat map
+	dynamic operator[](String key) => $meta.getTranslation(key);
+
+	late final Translations _root = this; // ignore: unused_field
+
+	Translations $copyWith({TranslationMetadata<AppLocale, Translations>? meta}) => Translations(meta: meta ?? this.$meta);
+
+	// Translations
+	late final Translations$pages$en pages = Translations$pages$en._(_root);
+}
+
+// Path: pages
+class Translations$pages$en {
+	Translations$pages$en._(this._root);
+
+	final Translations _root; // ignore: unused_field
+
+	// Translations
+	late final Translations$pages$login$en login = Translations$pages$login$en._(_root);
+}
+
+// Path: pages.login
+class Translations$pages$login$en {
+	Translations$pages$login$en._(this._root);
+
+	final Translations _root; // ignore: unused_field
+
+	// Translations
+
+	/// en: 'Connect'
+	String get connect => 'Connect';
+
+	/// en: 'Server URL'
+	String get server_url => 'Server URL';
+
+	/// en: 'Welcome'
+	String get welcome => 'Welcome';
+}
+
+/// The flat map containing all translations for locale <en>.
+/// Only for edge cases! For simple maps, use the map function of this library.
+///
+/// The Dart AOT compiler has issues with very large switch statements,
+/// so the map is split into smaller functions (512 entries each).
+extension on Translations {
+	dynamic _flatMapFunction(String path) {
+		return switch (path) {
+			'pages.login.connect' => 'Connect',
+			'pages.login.server_url' => 'Server URL',
+			'pages.login.welcome' => 'Welcome',
+			_ => null,
+		};
+	}
+}
--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
+import './gen/strings.g.dart';
+
 void main() {
-  runApp(const MainApp());
+  // set up and use translations
+  WidgetsFlutterBinding.ensureInitialized();
+  LocaleSettings.useDeviceLocale();
+  runApp(TranslationProvider(child: const MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -9,8 +14,22 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return const MaterialApp(home: LoginScreen());
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Image.asset("assets/logo.png", width: 50),
+          FilledButton(onPressed: () {}, child: Text(t.pages.login.connect)),
+        ],
+      ),
     );
   }
 }
