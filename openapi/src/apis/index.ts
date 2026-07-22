3e5b88fc7c52c680164c65abafad4ed0432e562b
Meow <github@xela.codes>
2026-07-22 14:45:36 -0400

    remove old openapi client

--- openapi/src/apis/index.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './ArtistsApi';
-export * from './DefaultApi';
-export * from './MusicApi';
-export * from './SearchApi';
--- openapi/src/models/index.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './Artist';
-export * from './ArtistSearchOutputBody';
-export * from './ArtistSearchRequest';
-export * from './ErrorDetail';
-export * from './ErrorModel';
-export * from './GreetingOutputBody';
