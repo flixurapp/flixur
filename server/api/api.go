6114873d964240dee27d1f83cc0c18c87901403d
Meow <github@xela.codes>
2026-07-23 13:33:47 -0400

    remove greeting

--- server/api/api.go
+++ server/api/api.go
@@ -1,9 +1,6 @@
 package api
 
 import (
-	"context"
-	"fmt"
-
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humachi"
 	"github.com/go-chi/chi/v5"
@@ -24,13 +21,6 @@ type InputPluginParamsOptional struct {
 	Plugin string `json:"plugin,omitempty" doc:"Plugin ID to use for the request. Omit to use the local server."`
 }
 
-// GreetingOutput represents the greeting operation response.
-type GreetingOutput struct {
-	Body struct {
-		Message string `json:"message" example:"Hello, world!" doc:"Greeting message"`
-	}
-}
-
 func RegisterAPI(router chi.Router) {
 	config := huma.DefaultConfig("Flixur API", "0.0.1")
 	config.Servers = []*huma.Server{{URL: "/api"}}
@@ -46,16 +36,18 @@ func RegisterAPI(router chi.Router) {
 
 		api := humachi.New(r, config)
 
+		RegisterAuthenticationRoutes(api)
 		RegisterMusicArtistsRoutes(api)
 
 		//TODO:testing
-		// Register GET /greeting/{name} handler.
-		huma.Get(api, "/greeting/{name}", func(ctx context.Context, input *struct {
-			Name string `path:"name" maxLength:"30" example:"world" doc:"Name to greet"`
-		}) (*GreetingOutput, error) {
-			resp := &GreetingOutput{}
-			resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
-			return resp, nil
-		})
+		/*
+			huma.Get(api, "/greeting/{name}", func(ctx context.Context, input *struct {
+				Name string `path:"name" maxLength:"30" example:"world" doc:"Name to greet"`
+			}) (*GreetingOutput, error) {
+				resp := &GreetingOutput{}
+				resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
+				return resp, nil
+			})
+		*/
 	})
 }
